bf9cc90b1d68241fcf145e6b667728be4a196a41
Zuddd95 <ahmadizzuddin@students.untidar.ac.id>
2026-08-23 12:56:05 +0700

    Add files via upload

---
+++ zudin.py
@@ -0,0 +1,21 @@
+
+# Menampilkan judul program
+print("\nDATA DIRI MAHASISWA")
+
+# Menampilkan nama mahasiswa
+print("\tNama \t\t\t\t\t: Ahmad Izzuddin")
+
+# Menampilkan NIM mahasiswa
+print("\tNIM \t\t\t\t\t: 2605060008")
+
+# Menampilkan tempat dan tanggal lahir
+print("\tTempat, Tanggal Lahir \t: Magelang, 22 November 2008")
+
+# Menampilkan alamat
+print("\tAlamat \t\t\t\t\t: Magelang, Jawa Tengah")
+
+# Menampilkan jenis kelamin
+print("\tJenis Kelamin \t\t\t: Laki-laki")
+
+# Menampilkan hobi
+print("\tHobi \t\t\t\t\t: Membaca dan Belajar")
